7752a6b82c533ca1d1e962bcc321f17cc7024134
GoGoMosh <KE16012001@yandex.ru>
2024-11-01 01:48:31 +0300

    Upgrade

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import requests as req
+from bs4 import BeautifulSoup
+# плюс lxml это используемый парсер
 
 # ссылка на сайт для парсинга(сбора данных со страницы сайты)
 link = 'https://browser-info.ru/'
 
 # получаем контент со страницы (text - указан для получения инфы на странице)
-ans = req.get(link)
+ans = req.get(link).text
+#
+# # проверяем дошёл ли наш запрос до сайта, если 200, то всё ок
+# print(ans.status_code)
+#
+# # вывод полученной инфы
+# print(ans.text)
+#
+# # запишем ответ в html файл
+# with open('ans.html', 'w', encoding='utf-8') as file:
+#    file.write(ans.text)
 
-# проверяем дошёл ли наш запрос до сайта, если 200, то всё ок
-print(ans.status_code)
-
-# вывод полученной инфы
-print(ans.text)
-
-# запишем ответ в html файл
-with open('ans.html', 'w', encoding='utf-8') as file:
-    file.write(ans.text)
+# для нахождения нужных значений на странице
+soup = BeautifulSoup(ans, 'lxml')
