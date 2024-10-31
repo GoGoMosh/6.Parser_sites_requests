dc69c5afc35b7fad5a7ba67f82c4b418aae3116e
GoGoMosh <KE16012001@yandex.ru>
2024-11-01 01:03:06 +0300

    Upgrade

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import requests
 
-# ссылка на сайт для парсинга(сбора данных со страницы сайты)
+# ссылка на сайт для парсинга(сбора данных со страницы сайты
 link = 'https://megamarket.ru/catalog/banki-dlya-hraneniya-produktov/'
