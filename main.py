ccc29c68bc1ee13a1be3f01cfc5681ae640260f1
GoGoMosh <KE16012001@yandex.ru>
2024-11-01 01:32:58 +0300

    Upgrade

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-import requests
+import requests as req
 
 # ссылка на сайт для парсинга(сбора данных со страницы сайты)
-link = 'https://megamarket.ru/catalog/banki-dlya-hraneniya-produktov/'
+link = 'https://browser-info.ru/'
+
+# получаем контент со страницы (text - указан для получения инфы на странице)
+ans = req.get(link)
+
+# проверяем дошёл ли наш запрос до сайта, если 200, то всё ок
+print(ans.status_code)
+
+# вывод полученной инфы
+print(ans.text)
+
+# запишем ответ в html файл
+with open('ans.html', 'w', encoding='utf-8') as file:
+    file.write(ans.text)
