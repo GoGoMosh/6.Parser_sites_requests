08ed77247b6083322359784aee0a0bce7278136e
GoGoMosh <KE16012001@yandex.ru>
2024-11-10 16:12:56 +0300

    Upgrade

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import multiprocessing
 import json
 
 
-# плюс lxml это используемый парсерррррр
+# плюс lxml это используемый парсер
 
 def get_js(sp):
     block = sp.find('div', id='javascript_check')
